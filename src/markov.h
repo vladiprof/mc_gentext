e033c6c01f4a8545b8e793f7dea1fc7ea7bbf1ec
vlad kamanov <vlad.kamanov@astrohn.ru>
2018-04-09 00:34:44 +0300

    MCM implementation not tested

--- src/markov.h
+++ src/markov.h
@@ -1,63 +1,110 @@
 #pragma once
 
-// TODO Int?
-template<Table>
-std::vector<int> FindStatesChain(const Table &log_transition_probs,
-                                 int state,
+#include <unordered_map>
+
+template<typename T>
+class SparseTable {
+private:
+    using RowIterator = typename std::unordered_map<size_t, T>::const_iterator;
+
+    class RangeIterator {
+    public:
+        RangeIterator(const std::unordered_map<size_t, T> &row)
+            : begin_(row.cbegin()), end_(row.cend()) {}
+
+        RowIterator begin() const {
+            return begin_;
+        }
+
+        RowIterator end() const {
+            return end_;
+        }
+    private:
+        RowIterator begin_;
+        RowIterator end_;
+    };
+public:
+    SparseTable(size_t rows_count, const T &default_value = T{}) : rows_(rows_count),
+                                                                   default_value_(default_value) {
+    }
+
+    T Get(size_t row, size_t col) const {
+        const auto &row_val = rows_[row];
+        auto it = row_val.find(col);
+
+        if (it != row_val.end()) {
+            return it->second;
+        } else {
+            return default_value_;
+        }
+    }
+
+    size_t GetRowsCount() const { return rows_.size(); }
+
+    void Set(size_t row, size_t col, const T &value) {
+        rows_[row][col] = value;
+    }
+
+    RangeIterator GetRow(size_t row) const {
+        return RangeIterator(rows_.at(row));
+    }
+
+    size_t MaxValueCol(size_t row) const {
+        const auto &row_value = rows_[row];
+
+        if (!row_value.empty()) {
+            return std::max_element(row_value.begin(), row_value.end(),
+                                    [](const std::pair<int, double> &lhs,
+                                       const std::pair<int, double> &rhs) {
+                                        return lhs.second < rhs.second;
+                                    })->first;
+        } else {
+            return default_value_;
+        }
+    }
+
+private:
+    std::vector<std::unordered_map<size_t, T>> rows_;
+    T default_value_;
+};
+
+std::vector<int> FindStatesChain(const SparseTable<double> &log_transition_probs,
+                                 int current_state,
                                  size_t chain_length) {
-  // TODO
-  Table s(log_transition_probs.rows());
+    SparseTable<double> dyntbl(chain_length, std::numeric_limits<double>::lowest());
+    SparseTable<double> dyntbl_states(chain_length);
 
-  //tr.first -> col
-  for (const auto &tr : log_transition_probs[state]) {
-    s[0][tr.first] = tr.second;
-  }
-
-  for (size_t id = 1; id < chain_length; ++id) {
-    for (const auto &s_tr : s[id - 1]) {
-      for (const auto& ) {
-        s[
-      }
+    for (const auto &next_state: log_transition_probs.GetRow(current_state)) {
+        dyntbl.Set(0, next_state.first, next_state.second);
+        dyntbl_states.Set(0, next_state.first, current_state);
     }
-  }
 
-}
-
-
-// Brute force
-// T = [[0.5, 0.3, 0.0]  -> 0
-//      [0.0, 0.4, 0.3]  -> 1
-//      [0.1, 0.2, 0.4]] -> 3
-
-std::vector<int> BruteFindStatesChain(double transition_probs[],
-                                      int state,
-                                      size_t chain_length,
-                                      size_t states_count,
-                                      double &chain_prob) {
-  if (chain_length == 0) {
-    chain_prob = 1.0;
-    return {};
-  }
-
-  double max_prob = 0;
-  std::vector<int> max_prob_chain{};
-
-  for (int next_state = 0; next_state < states_count; next_state++) {
-    double chain_prob = 0.0;
-    double
-        transition_prob = transition_probs[state * states_count + next_state];
-    auto &chain = FindStatesChain(transition_probs,
-                                  next_state,
-                                  chain_length - 1,
-                                  states_count,
-                                  chain_prob);
-
-    if (chain_prob * transition_prob > max_prob) {
-      max_prob_chain = std::move(chain);
-      max_prob = chain_prob * transition_prob;
+    for (size_t state = 1; state < chain_length; ++state) {
+        for (const auto &inter_state : dyntbl.GetRow(state - 1)) {
+            for (const auto &next_state : log_transition_probs.GetRow(inter_state.first)) {
+                double prob = inter_state.second + next_state.second;
+                if (prob > dyntbl.Get(state, next_state.second)) {
+                    dyntbl.Set(state, next_state.first, prob);
+                    dyntbl_states.Set(state, next_state.first, inter_state.first);
+                }
+            }
+        }
     }
-  }
 
-  chain_prob = max_prob;
-  return max_prob_chain;
+    // Reconstruct chain
+
+    std::vector<int> states_chain(chain_length);
+    auto it_out = states_chain.rbegin();
+
+    int state = dyntbl.MaxValueCol(chain_length - 1);
+    *(it_out++) = state;
+    --chain_length;
+
+    for (; chain_length > 0; --chain_length) {
+        state = dyntbl_states.Get(chain_length, state);
+        *(it_out++) = state;
+    }
+
+
+    return states_chain;
 }
--- tests/markov_chain_tests.cpp
+++ tests/markov_chain_tests.cpp
@@ -1,5 +1,185 @@
 #include <gtest/gtest.h>
 
-TEST(Some, Some) {
-  ASSERT_EQ(1, 1);
+#include "markov.h"
+
+std::vector<int> BruteFindStatesChain(const double transition_probs[],
+                                      int state,
+                                      size_t chain_length,
+                                      size_t states_count,
+                                      double &chain_prob);
+
+double CalculateProbability(const double transition_probs[],
+                            int state,
+                            size_t states_count,
+                            const std::vector<int> &chain);
+
+std::unique_ptr<double[]> GenerateTransitionsMatrix(size_t size);
+
+TEST(Markov, CalculateProbability) {
+    const double transition[] = {0.0, 0.6, 0.4,
+                                 0.5, 0.5, 0.0,
+                                 0.0, 0.99, 0.01};
+
+    ASSERT_EQ(0.3, CalculateProbability(transition, 0, 3, {1, 0}));
+}
+
+TEST(Markov, CheckBruteforce) {
+    const double transition[] = {0.0, 0.6, 0.4,
+                                 0.5, 0.5, 0.0,
+                                 0.0, 0.99, 0.01};
+
+    double prob;
+
+    ASSERT_EQ(std::vector<int>({2, 1}), BruteFindStatesChain(transition, 0, 2, 3, prob));
+    ASSERT_EQ(0.4 * 0.99, prob);
+}
+
+SparseTable<double> GetSparseTable(const double *source, size_t size) {
+    SparseTable<double> table(size, std::numeric_limits<double>::lowest());
+
+    for (size_t row = 0; row < size; ++row) {
+        for (size_t col = 0; col < size; ++col) {
+            if (*source != 0.0) {
+                table.Set(row, col, std::log(*source));
+            }
+            ++source;
+        }
+    }
+
+    return table;
+}
+
+TEST(Markov, FindStatesChain1) {
+    const double transition[] = {0.0, 0.6, 0.4,
+                                 0.5, 0.5, 0.0,
+                                 0.0, 0.99, 0.01};
+
+    const auto& table = GetSparseTable(transition, 3);
+    ASSERT_EQ(std::vector<int>({2, 1}), FindStatesChain(table, 0, 2));
+}
+
+TEST(Markov, FindStatesChain2) {
+    const double transition[] = {0.0, 0.6, 0.4,
+                                 0.5, 0.5, 0.0,
+                                 0.0, 0.99, 0.01};
+
+    const auto& table = GetSparseTable(transition, 3);
+    double prob;
+    ASSERT_EQ(BruteFindStatesChain(transition, 0, 3, 3, prob), FindStatesChain(table, 0, 3));
+}
+
+TEST(Markov, FindStatesChain3) {
+    const double transition[] = {0.0, 0.6, 0.4,
+                                 0.5, 0.5, 0.0,
+                                 0.0, 0.99, 0.01};
+
+    const auto& table = GetSparseTable(transition, 3);
+    ASSERT_EQ(std::vector<int>({1}), FindStatesChain(table, 0, 1));
+}
+
+// TODO Print
+
+TEST(Markov, FindStatesChain4) {
+    const size_t transitions_count = 5;
+    const size_t chain_size = 2;
+
+    double prob;
+    for (size_t attempts = 0; attempts < 100; ++attempts) {
+        const auto& transitions = GenerateTransitionsMatrix(transitions_count);
+        const auto& table = GetSparseTable(transitions.get(), transitions_count);
+
+        const auto& expected = BruteFindStatesChain(transitions.get(), 0, chain_size, transitions_count, prob);
+        const auto& actual = FindStatesChain(table, 0, chain_size);
+
+        ASSERT_EQ(CalculateProbability(transitions.get(), 0, transitions_count, expected),
+                  CalculateProbability(transitions.get(), 0, transitions_count, actual));
+    }
+}
+
+
+
+std::vector<int> RecursiveBruteFindStatesChain(const double transition_probs[],
+                                               int state,
+                                               size_t chain_length,
+                                               size_t states_count,
+                                               double &chain_prob) {
+    if (chain_length == 0) {
+        chain_prob = 1.0;
+        return {};
+    }
+
+    double max_prob = 0;
+    std::vector<int> max_prob_chain{};
+
+    for (int next_state = 0; next_state < states_count; next_state++) {
+        double chain_prob = 0.0;
+        double
+            transition_prob = transition_probs[state * states_count + next_state];
+        auto chain = RecursiveBruteFindStatesChain(transition_probs,
+                                                   next_state,
+                                                   chain_length - 1,
+                                                   states_count,
+                                                   chain_prob);
+
+        if (chain_prob * transition_prob > max_prob) {
+            max_prob_chain = std::move(chain);
+            max_prob_chain.push_back(next_state);
+            max_prob = chain_prob * transition_prob;
+        }
+    }
+
+    chain_prob = max_prob;
+    return max_prob_chain;
+}
+
+std::vector<int> BruteFindStatesChain(const double transition_probs[],
+                                      int state,
+                                      size_t chain_length,
+                                      size_t states_count,
+                                      double &chain_prob) {
+    auto chain = RecursiveBruteFindStatesChain(transition_probs,
+                                               state,
+                                               chain_length,
+                                               states_count,
+                                               chain_prob);
+
+    std::reverse(chain.begin(), chain.end());
+    return chain;
+}
+
+double CalculateProbability(const double transition_probs[],
+                            int state,
+                            size_t states_count,
+                            const std::vector<int> &chain) {
+    int prev_state = state;
+    double probability = 1.0;
+
+    for (const auto &node : chain) {
+        probability *= transition_probs[prev_state * states_count + node];
+        prev_state = node;
+    }
+
+    return probability;
+}
+
+std::unique_ptr<double[]> GenerateTransitionsMatrix(size_t size) {
+    std::unique_ptr<double[]> matrix(new double[size * size]);
+    std::mt19937 gen(3312444);
+    std::uniform_real_distribution<double> distr(0.0, 1.0);
+    auto matrix_ptr = matrix.get();
+
+    for (size_t row = 0; row < size; ++row) {
+        double prob_sum = 0.0;
+        for (size_t col = 0; col < size; ++col) {
+            double el = distr(gen);
+            prob_sum += el;
+            *(matrix_ptr++) = el;
+        }
+
+        for (size_t col = 1; col <= size; ++col) {
+            *(matrix_ptr - col) /= prob_sum;
+        }
+    }
+
+    return matrix;
 }
